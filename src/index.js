2bb3b4cfa0169867cec16fb6d6508eb15a9415f0
cuibai <2367736060@qq.com>
2022-10-08 15:04:03 +0800

    1008

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: cuibai 2367736060@qq.com
+ * @Date: 2022-10-08 10:34:26
+ * @LastEditors: cuibai 2367736060@qq.com
+ * @LastEditTime: 2022-10-08 14:43:53
+ * @FilePath: \webpack-s\src\index.js
+ * @Description: //
+ * 
+ * Copyright (c) 2022 by cuibai 2367736060@qq.com, All Rights Reserved. 
+ */
+// 使用ES6 导入
+import $ from 'jquery'
